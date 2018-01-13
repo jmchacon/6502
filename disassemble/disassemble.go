ae2d939767059d3b83e97ee0a455478c998ee445
James Chacon <chacon.james@gmail.com>
2018-01-13 19:27:23 +0000

    Add support for auto detect of a c64 basic program and link in the c64basic package to decompile it. Once it's done, skip over the last NUL basic line and start disassembling from here until we're out of bytes that loaded.

--- disassemble/disassemble.go
+++ disassemble/disassemble.go
@@ -1,5 +1,10 @@
 // disassenble takes a filename and load's it and then
 // disassembles it to stdout starting at the first instruction.
+// If the filename ends in .prg (case insensitive) it will assume
+// this is a C64 program file and use the first 2 bytes as the load
+// address. If the load address is 0x0801 it will then assume it's
+// BASIC program and start listing it until it ends. At that point it'll
+// disassemble until the end of the load address space.
 package main
 
 import (
@@ -8,33 +13,26 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/jmchacon/6502/c64basic"
 	"github.com/jmchacon/6502/cpu"
 )
 
 // flatMemory implements the RAM interface
 type flatMemory struct {
-	addr []uint8
+	addr [65536]uint8
 }
 
 func (r *flatMemory) Read(addr uint16) uint8 {
-	if addr >= uint16(len(r.addr)) {
-		return 0x00
-	}
 	return r.addr[addr]
 }
 
 func (r *flatMemory) ReadAddr(addr uint16) uint16 {
-	if addr >= uint16(len(r.addr)) || addr+1 >= uint16(len(r.addr)) {
-		return 0x0000
-	}
 	return (uint16(r.addr[addr+1]) << 8) + uint16(r.addr[addr])
 }
 
 func (r *flatMemory) ReadZPAddr(addr uint8) uint16 {
-	if addr >= uint8(len(r.addr)) || addr+1 >= uint8(len(r.addr)) {
-		return 0x0000
-	}
 	return (uint16(r.addr[addr+1]) << 8) + uint16(r.addr[addr])
 }
 
@@ -45,7 +43,7 @@ func (r *flatMemory) PowerOn() {}
 
 var (
 	startPC = flag.Int("start_pc", 0x0000, "PC value to start disassembling")
-	offset  = flag.Int("offset", 0x0000, "Offset into RAM to start loading data. All other RAM will be zero'd out.")
+	offset  = flag.Int("offset", 0x0000, "Offset into RAM to start loading data. All other RAM will be zero'd out. Ignored for PRG files.")
 )
 
 func main() {
@@ -54,25 +52,59 @@ func main() {
 		log.Fatalf("Invalid command: %s [-start_pc <PC> -offset <offset>] <filename>", os.Args[0])
 	}
 	fn := flag.Args()[0]
+
+	// Check if this is a c64 binary.
+	c64 := false
+	parts := strings.Split(fn, ".")
+	suffix := strings.ToLower(parts[len(parts)-1])
+	if suffix == "prg" {
+		c64 = true
+		fmt.Println("C64 program file")
+	}
+
 	f := &flatMemory{}
+	f.PowerOn()
 	var err error
 	b, err := ioutil.ReadFile(fn)
 	if err != nil {
 		log.Fatalf("Can't open %s - %v", fn, err)
 	}
-	for i := 0; i < *offset; i++ {
-		f.addr = append(f.addr, 0)
+	pc := uint16(*startPC)
+	if c64 {
+		// We're supplied with the load offset instead of using the flag (which we'll override).
+		*offset = int((uint16(b[1]) << 8) + uint16(b[0]))
+		pc = uint16(*offset)
+		*startPC = int(pc)
+		b = b[2:]
 	}
-	f.addr = append(f.addr, b...)
-	l := len(b)
-	if l > 65536-*offset {
+	max := 65536 - *offset
+	if l := len(b); l > 65536-*offset {
 		log.Printf("Length %d at offset %d too long, truncating to 64k", l, *offset)
-		f.addr = f.addr[:65536-*offset]
+		b = b[:max]
 	}
-	fmt.Printf("0x%.2X bytes\n", l)
-	for pc := *startPC; pc < (*startPC + l); {
-		dis, off := cpu.Disassemble(uint16(pc), f)
-		pc += off
+	fmt.Printf("0x%.2X bytes\n", len(b))
+	copy(f.addr[*offset:], b)
+	if c64 && *offset == 0x0801 {
+		// Start with basic first
+		for {
+			out, newPC, err := c64basic.List(pc, f)
+			if newPC == 0x0000 {
+				// Account for 3 NULs indicating end of program
+				pc += 2
+				fmt.Printf("PC: %.4X\n", pc)
+				break
+			}
+			fmt.Printf("%.4X %s\n", pc, out)
+			if err != nil {
+				fmt.Printf("%v", err)
+				os.Exit(1)
+			}
+			pc = newPC
+		}
+	}
+	for pc < uint16(*startPC+len(b)) {
+		dis, off := cpu.Disassemble(pc, f)
+		pc += uint16(off)
 		fmt.Printf("%s\n", dis)
 	}
 }
